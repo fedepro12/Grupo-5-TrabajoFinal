d0f03cb674c7a83be4836fb80cfbe0fc745bab51
fedepro12 <142936905+fedepro12@users.noreply.github.com>
2023-11-28 16:20:44 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+var button  = document.querySelector("#botao-teste")
+console.log(button)
+button.addEventListener("click", function(){
+  var divs = document.querySelector("#pergunta")
+  console.log(divs)
+  
+})
+
